42ff6592b283701e551d96b20efdb45c174a6c85
Laura Weston <laura.weston@outlook.com>
2016-09-21 19:55:27 +0100

    Lawn takes width and length as parameters when initialized

--- spec/lawnmowers_spec.rb
+++ spec/lawnmowers_spec.rb
@@ -3,17 +3,21 @@ require 'lawnmowers'
 class Lawn
   attr_reader :width, :length
 
-  def initialize
-    @width = 0
-    @length = 0
+  def initialize(x, y)
+    @width = x
+    @length = y
   end
 end
 
-
 describe 'lawnmowers' do
   it 'creates a lawn of size 0,0' do
-    lawn = Lawn.new
+    lawn = Lawn.new(0,0)
     expect(lawn.width).to eq 0
     expect(lawn.length).to eq 0
+  end
+  it 'creates a lawn of size 1,1' do
+    lawn = Lawn.new(1,1)
+    expect(lawn.width).to eq 1
+    expect(lawn.length).to eq 1
   end
 end
