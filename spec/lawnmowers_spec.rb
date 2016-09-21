0a6ca2ecc53619c331a5de81ea237a18c3d6b231
Laura Weston <laura.weston@outlook.com>
2016-09-21 20:14:52 +0100

    Creates a lawn of size x,y parameters

--- spec/lawnmowers_spec.rb
+++ spec/lawnmowers_spec.rb
@@ -2,7 +2,6 @@ require 'lawnmowers'
 
 class Lawn
   attr_reader :width, :length
-
   def initialize(x, y)
     @width = x
     @length = y
@@ -10,14 +9,30 @@ class Lawn
 end
 
 describe 'lawnmowers' do
-  it 'creates a lawn of size 0,0' do
-    lawn = Lawn.new(0,0)
-    expect(lawn.width).to eq 0
-    expect(lawn.length).to eq 0
+
+  describe 'lawn' do
+    it 'can create a lawn of size 0,0' do
+      lawn = Lawn.new(0,0)
+      expect(lawn.width).to eq 0
+      expect(lawn.length).to eq 0
+    end
+    it 'can create a lawn of size 1,1' do
+      lawn = Lawn.new(1,1)
+      expect(lawn.width).to eq 1
+      expect(lawn.length).to eq 1
+    end
+    it 'can create a lawn of size 1,2' do
+      lawn = Lawn.new(1,2)
+      expect(lawn.width).to eq 1
+      expect(lawn.length).to eq 2
+    end
+    it 'can create a lawn of size 2,1' do
+      lawn = Lawn.new(2,1)
+      expect(lawn.width).to eq 2
+      expect(lawn.length).to eq 1
+    end
   end
-  it 'creates a lawn of size 1,1' do
-    lawn = Lawn.new(1,1)
-    expect(lawn.width).to eq 1
-    expect(lawn.length).to eq 1
-  end
+
+
+
 end
